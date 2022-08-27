8227d5d832f9af227557cf3f0a76b1c186cf2380
Glauber Santana <glauber.silva14@gmail.com>
2022-08-26 22:07:35 -0300

    feat: atualizacao das rotas compponentes e paginas individuais

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "normalize.css";
-import ".index.css";
-import Router from "./routes";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css';
+import './index.css';
+import Router from './routes';
 
 ReactDOM.render(
   <React.StrictMode>
-      <Router />
+    <Router />
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,5 +1,4 @@
 import styles from "./Cardapio.module.scss";
-import { ReactComponent as Logo } from "assets/logo.svg";
 import Buscador from "./Buscador";
 import { useState } from "react";
 import Filtros from "./Filtros";
@@ -11,15 +10,6 @@ export default function Cardapio() {
   const [filtro, setFiltro] = useState<number | null>(null);
   const [ordenador, setOrdenador] = useState("");
   return (
-    <main>
-      <nav className={styles.menu}>
-        <Logo />
-      </nav>
-      <header className={styles.header}>
-        <div className={styles.header__text}>
-          A casa do código e da massa
-        </div>
-      </header>
       <section className={styles.cardapio}>
         <h3 className={styles.cardapio__titulo}>Cardápio</h3>
         <Buscador busca={busca} setBusca={setBusca} />
@@ -29,6 +19,5 @@ export default function Cardapio() {
         </div>
         <Itens busca={busca} filtro={filtro} ordenador={ordenador} />
       </section>
-    </main>
   )
 }
